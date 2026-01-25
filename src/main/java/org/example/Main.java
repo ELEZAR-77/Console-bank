8d08a6435fce35d980bf6d5e6e4818db4fd62119
ELEZAR-77 <omarabdullaev68@gmail.com>
2026-01-25 23:45:00 +0300

    сделал слушатель консоли в отдельном потоке

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,6 +13,7 @@ public class Main {
 
         OperationsConsoleListener listener = context.getBean(OperationsConsoleListener.class);
 
-        listener.run();
+        Thread consoleThread = new Thread(listener);
+        consoleThread.start();
     }
 }
